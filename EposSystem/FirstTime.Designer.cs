a062b230bdaac7844c2dd56ef78a58dd33c03b5c
Drkongy <80266694+Drkongy@users.noreply.github.com>
2022-10-10 22:00:52 +0100

    Add project files.

---
+++ EposSystem/FirstTime.Designer.cs
@@ -0,0 +1,262 @@
+﻿using System.Windows.Forms;
+
+namespace EposSystem
+{
+    partial class FirstTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PBIcon = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TBOrg = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TBBranch = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TBEmail = new System.Windows.Forms.TextBox();
+            this.BtnSubmit = new System.Windows.Forms.Button();
+            this.LblError = new System.Windows.Forms.Label();
+            this.LblCode = new System.Windows.Forms.Label();
+            this.TBCode = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TBPassword = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PBIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(91, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(590, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Initializing Kongolian Systems, Please Fill Spaces Provided.";
+            // 
+            // TBName
+            // 
+            this.TBName.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.TBName.Location = new System.Drawing.Point(95, 111);
+            this.TBName.Name = "TBName";
+            this.TBName.Size = new System.Drawing.Size(506, 27);
+            this.TBName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(304, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // PBIcon
+            // 
+            this.PBIcon.Image = global::EposSystem.Properties.Resources.Icon;
+            this.PBIcon.Location = new System.Drawing.Point(10, 10);
+            this.PBIcon.Name = "PBIcon";
+            this.PBIcon.Size = new System.Drawing.Size(75, 75);
+            this.PBIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PBIcon.TabIndex = 3;
+            this.PBIcon.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(271, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 30);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Organization";
+            // 
+            // TBOrg
+            // 
+            this.TBOrg.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.TBOrg.Location = new System.Drawing.Point(95, 174);
+            this.TBOrg.Name = "TBOrg";
+            this.TBOrg.Size = new System.Drawing.Size(506, 27);
+            this.TBOrg.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(301, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 30);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Branch";
+            // 
+            // TBBranch
+            // 
+            this.TBBranch.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.TBBranch.Location = new System.Drawing.Point(94, 237);
+            this.TBBranch.Name = "TBBranch";
+            this.TBBranch.Size = new System.Drawing.Size(506, 27);
+            this.TBBranch.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(304, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 30);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Email";
+            // 
+            // TBEmail
+            // 
+            this.TBEmail.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.TBEmail.Location = new System.Drawing.Point(95, 300);
+            this.TBEmail.Name = "TBEmail";
+            this.TBEmail.Size = new System.Drawing.Size(505, 27);
+            this.TBEmail.TabIndex = 10;
+            // 
+            // BtnSubmit
+            // 
+            this.BtnSubmit.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.BtnSubmit.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.BtnSubmit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BtnSubmit.Location = new System.Drawing.Point(94, 396);
+            this.BtnSubmit.Name = "BtnSubmit";
+            this.BtnSubmit.Size = new System.Drawing.Size(507, 36);
+            this.BtnSubmit.TabIndex = 12;
+            this.BtnSubmit.Text = "Submit";
+            this.BtnSubmit.UseVisualStyleBackColor = false;
+            // 
+            // LblError
+            // 
+            this.LblError.AutoSize = true;
+            this.LblError.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.LblError.ForeColor = System.Drawing.Color.IndianRed;
+            this.LblError.Location = new System.Drawing.Point(95, 315);
+            this.LblError.Name = "LblError";
+            this.LblError.Size = new System.Drawing.Size(0, 25);
+            this.LblError.TabIndex = 13;
+            // 
+            // LblCode
+            // 
+            this.LblCode.AutoSize = true;
+            this.LblCode.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold);
+            this.LblCode.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblCode.Location = new System.Drawing.Point(144, 330);
+            this.LblCode.Name = "LblCode";
+            this.LblCode.Size = new System.Drawing.Size(135, 30);
+            this.LblCode.TabIndex = 15;
+            this.LblCode.Text = "Admin Code";
+            // 
+            // TBCode
+            // 
+            this.TBCode.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.TBCode.Location = new System.Drawing.Point(95, 363);
+            this.TBCode.Name = "TBCode";
+            this.TBCode.Size = new System.Drawing.Size(243, 27);
+            this.TBCode.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(412, 330);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 30);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Password";
+            // 
+            // TBPassword
+            // 
+            this.TBPassword.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.TBPassword.Location = new System.Drawing.Point(343, 363);
+            this.TBPassword.Name = "TBPassword";
+            this.TBPassword.Size = new System.Drawing.Size(257, 27);
+            this.TBPassword.TabIndex = 16;
+            // 
+            // FirstTime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(720, 480);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.TBPassword);
+            this.Controls.Add(this.LblCode);
+            this.Controls.Add(this.TBCode);
+            this.Controls.Add(this.LblError);
+            this.Controls.Add(this.BtnSubmit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TBEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TBBranch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TBOrg);
+            this.Controls.Add(this.PBIcon);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TBName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FirstTime";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FirstTime";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.PBIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox TBName;
+        private Label label2;
+        private PictureBox PBIcon;
+        private Label label3;
+        private TextBox TBOrg;
+        private Label label4;
+        private TextBox TBBranch;
+        private Label label5;
+        private TextBox TBEmail;
+        private Button BtnSubmit;
+        private Label LblError;
+        private Label LblCode;
+        private TextBox TBCode;
+        private Label label7;
+        private TextBox TBPassword;
+    }
+}
---
+++ EposSystem/SplashScreen.Designer.cs
@@ -0,0 +1,150 @@
+﻿using System.Windows.Forms;
+
+namespace EposSystem
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this.PnlMain = new System.Windows.Forms.Panel();
+            this.lblLisence = new System.Windows.Forms.Label();
+            this.lblLabel2 = new System.Windows.Forms.Label();
+            this.lblIogo1 = new System.Windows.Forms.Label();
+            this.PBIcon = new System.Windows.Forms.PictureBox();
+            this.LblLoadingMsgs = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.PnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PnlMain
+            // 
+            this.PnlMain.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PnlMain.Controls.Add(this.lblLisence);
+            this.PnlMain.Controls.Add(this.lblLabel2);
+            this.PnlMain.Controls.Add(this.lblIogo1);
+            this.PnlMain.Controls.Add(this.PBIcon);
+            this.PnlMain.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.PnlMain.Location = new System.Drawing.Point(17, 17);
+            this.PnlMain.Name = "PnlMain";
+            this.PnlMain.Size = new System.Drawing.Size(583, 347);
+            this.PnlMain.TabIndex = 0;
+            // 
+            // lblLisence
+            // 
+            this.lblLisence.AutoSize = true;
+            this.lblLisence.Location = new System.Drawing.Point(17, 325);
+            this.lblLisence.Name = "lblLisence";
+            this.lblLisence.Size = new System.Drawing.Size(126, 13);
+            this.lblLisence.TabIndex = 3;
+            this.lblLisence.Text = "Licensed To: McDonalds";
+            // 
+            // lblLabel2
+            // 
+            this.lblLabel2.AutoSize = true;
+            this.lblLabel2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold);
+            this.lblLabel2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblLabel2.Location = new System.Drawing.Point(349, 218);
+            this.lblLabel2.Name = "lblLabel2";
+            this.lblLabel2.Size = new System.Drawing.Size(91, 30);
+            this.lblLabel2.TabIndex = 2;
+            this.lblLabel2.Text = "Systems";
+            // 
+            // lblIogo1
+            // 
+            this.lblIogo1.AutoSize = true;
+            this.lblIogo1.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Bold);
+            this.lblIogo1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblIogo1.Location = new System.Drawing.Point(39, 107);
+            this.lblIogo1.Name = "lblIogo1";
+            this.lblIogo1.Size = new System.Drawing.Size(514, 128);
+            this.lblIogo1.TabIndex = 1;
+            this.lblIogo1.Text = "Kongolian";
+            // 
+            // PBIcon
+            // 
+            this.PBIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.PBIcon.Image = global::EposSystem.Properties.Resources.Icon;
+            this.PBIcon.Location = new System.Drawing.Point(17, 17);
+            this.PBIcon.Name = "PBIcon";
+            this.PBIcon.Size = new System.Drawing.Size(86, 87);
+            this.PBIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PBIcon.TabIndex = 0;
+            this.PBIcon.TabStop = false;
+            // 
+            // LblLoadingMsgs
+            // 
+            this.LblLoadingMsgs.AutoSize = true;
+            this.LblLoadingMsgs.Location = new System.Drawing.Point(34, 380);
+            this.LblLoadingMsgs.Name = "LblLoadingMsgs";
+            this.LblLoadingMsgs.Size = new System.Drawing.Size(75, 13);
+            this.LblLoadingMsgs.TabIndex = 1;
+            this.LblLoadingMsgs.Text = "Loading Users";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(531, 380);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(72, 13);
+            this.lblVersion.TabIndex = 2;
+            this.lblVersion.Text = "Version: 1.0.0";
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(617, 416);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.LblLoadingMsgs);
+            this.Controls.Add(this.PnlMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StartUp";
+            this.PnlMain.ResumeLayout(false);
+            this.PnlMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel PnlMain;
+        private Label lblLabel2;
+        private Label lblIogo1;
+        private PictureBox PBIcon;
+        private Label lblLisence;
+        private Label LblLoadingMsgs;
+        private Label lblVersion;
+    }
+}
